29759e5f310c468138333493e2d7e9659452a92d
mofath <mohamed.elgaedi@gmail.com>
2021-02-14 09:10:53 +0200

    [client]: fetching data from client

---
+++ client/src/requests.js
@@ -0,0 +1,23 @@
+
+const endPointURL = "http://localhost:9000/graphql";
+
+export async function loadJobs() {
+  const response = await fetch(endPointURL, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      query: `{
+            jobs{
+                id
+                title
+                company{
+                    id
+                    name
+                }
+            }
+        }`,
+    }),
+  });
+  const responseBody = await response.json();
+  return responseBody.data.jobs;
+}
