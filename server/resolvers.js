2e601a1fa8b81dc605aed3e553ce7f3b9147faa4
mofath <mohamed.elgaedi@gmail.com>
2021-02-14 09:35:29 +0200

    [server] : Added qury to fetch job by id

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,13 +1,15 @@
-const db = require('./db');
+const db = require("./db");
 
 const Query = {
-    jobs: () => db.jobs.list(),
-}
+  job: (root, args) => db.jobs.get(args.id),
+  jobs: () => db.jobs.list(),
+};
 
 const Job = {
-    company : (job) => db.companies.get(job.companyId) 
-}
+  company: (job) => db.companies.get(job.companyId),
+};
 
 module.exports = {
-    Query, Job
-}
+  Query,
+  Job,
+};
