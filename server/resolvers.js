54597b328be0b6258dd75acce0e587a2bf95e70c
mofath <mohamed.elgaedi@gmail.com>
2021-02-17 13:29:53 +0200

    [server] : fetch jobs for each company

--- server/resolvers.js
+++ server/resolvers.js
@@ -10,7 +10,13 @@ const Job = {
   company: (job) => db.companies.get(job.companyId),
 };
 
+const Company = {
+  jobs: (company) =>
+    db.jobs.list().filter((job) => job.companyId === company.id),
+};
+
 module.exports = {
   Query,
   Job,
+  Company
 };
