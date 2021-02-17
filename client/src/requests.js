f0d0dc608bedea05fa1feb3ec59d76f5c3904f76
mofath <mohamed.elgaedi@gmail.com>
2021-02-17 02:29:44 +0200

    [client]: fretching job by id

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,4 +1,3 @@
-
 const endPointURL = "http://localhost:9000/graphql";
 
 export async function loadJobs() {
@@ -20,4 +19,28 @@ export async function loadJobs() {
   });
   const responseBody = await response.json();
   return responseBody.data.jobs;
+}
+
+export async function loadJob(id) {
+  const response = await fetch(endPointURL, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      query: `query JobQuery($id: ID!){
+            job(id: $id){
+                id
+                title
+                company{
+                    id
+                    name
+                }
+                description
+            }
+        }`,
+      variables: { id },
+    }),
+  });
+  const responseBody = await response.json();
+  console.log(responseBody);
+  return responseBody.data.job;
 }
